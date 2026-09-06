117b1c04c6b5f3156aa9a282ecb471d146b4ea5a
saju3018 <saju30183@gmail.com>
2026-09-06 12:04:15 +0600

    Update fork.js

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -6,7 +6,7 @@ module.exports = {
     version: "1.0",
     author: "Anik Islam Sadik",
     countDown: 3,
-    role: 0,
+    role: 2,
     category: "utility",
     shortDescription: "GitHub Fork Info",
     longDescription: "Fetch repository fork details",
